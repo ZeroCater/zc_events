945f87c14b69546133551badf0e5b3084d4a9409
Tony Pilara <tony.pilara@zerocater.com>
2021-01-21 17:46:46 -0800

    Lossen celery version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     ],
     install_requires=[
         'boto3>=1.4.7',
-        'celery>=3.1.10,<4.0.0',
+        'celery>=3.1.10',
         'inflection>=0.3.1,<0.4',
         'pika>=0.10.0,<0.11.0',
         'pika_pool>=0.1.3,<0.1.4',
