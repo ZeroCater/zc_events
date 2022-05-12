4e301f047387ea689749d321ae4745b3abb498bd
Cody Towstik <cody.towstik@zerocater.com>
2022-05-12 14:59:27 -0500

    * change version to 0.4.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_packages(package):
 
 setup(
     name='zc_events',
-    version='0.3.10',
+    version='0.4.0',
     description="Shared code for ZeroCater microservices events",
     long_description='',
     keywords='zerocater python util',
